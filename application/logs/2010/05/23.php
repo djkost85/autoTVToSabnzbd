c3c957f8e4e6a0a22869ba7c3b973b542a25f594
Erik Morén <morre95@hotmail.com>
2010-06-29 12:51:34 +0000

    Deleting logs and cache files

--- application/logs/2010/05/23.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2010-05-23 17:59:19 --- ERROR: ReflectionException [ 0 ]: Method action_episode does not exist ~ SYSPATH/classes\kohana\request.php [ 882 ]
-2010-05-23 17:59:23 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: episodes/index.php/http:/thetvdb.com/banners/124 ~ SYSPATH/classes\kohana\request.php [ 579 ]
-2010-05-23 17:59:34 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: episodes/index.php/http:/thetvdb.com/banners/124 ~ SYSPATH/classes\kohana\request.php [ 579 ]
-2010-05-23 17:59:47 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: episodes/index.php/http:/thetvdb.com/banners/124 ~ SYSPATH/classes\kohana\request.php [ 579 ]
-2010-05-23 18:00:17 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: episodes/index.php/http:/thetvdb.com/banners/124 ~ SYSPATH/classes\kohana\request.php [ 579 ]
-2010-05-23 23:17:07 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes\kohana\request.php [ 579 ]
--- application/logs/2010/06/19.php
+++
@@ -1,4 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2010-06-19 19:51:23 --- ERROR: ErrorException [ 2 ]: mysql_connect() [function.mysql-connect]: php_network_getaddresses: getaddrinfo failed: Ingen sådan värd är känd.  ~ MODPATH/database\classes\kohana\database\mysql.php [ 56 ]
-2010-06-19 20:40:48 --- ERROR: ReflectionException [ 0 ]: Method action_episode does not exist ~ SYSPATH/classes\kohana\request.php [ 882 ]
